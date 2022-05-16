873587dd8d70dd00cdb411ccc0e8fc640d46b495
agoenks29D <agungmasda29@gmail.com>
2022-05-17 00:54:05 +0700

    latest update

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2022 at 08:05 AM
+-- Generation Time: May 16, 2022 at 04:37 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -61,6 +61,16 @@ CREATE TABLE `rekam-medis` (
   `id` int(4) NOT NULL,
   `pasien` int(4) NOT NULL,
   `jadwal` int(4) NOT NULL,
+  `tekanan_darah` varchar(100) NOT NULL,
+  `nadi` varchar(100) NOT NULL,
+  `pernafasan` varchar(100) NOT NULL,
+  `suhu` varchar(100) NOT NULL,
+  `sistem_pencernaan` varchar(100) NOT NULL,
+  `jantung_pembuluh_darah` varchar(100) NOT NULL,
+  `sistem_saraf_pusat` varchar(100) NOT NULL,
+  `tht_dan_kulit` varchar(100) NOT NULL,
+  `keterangan` varchar(100) DEFAULT NULL,
+  `pemeriksaan_urin` varchar(100) NOT NULL,
   `keluhan` text DEFAULT NULL,
   `diagnosis` text DEFAULT NULL,
   `saran` text DEFAULT NULL,
@@ -91,7 +101,8 @@ CREATE TABLE `user` (
   `education` enum('NONE','SD','SLTP','SLTA','D3','S1','S2','S3') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `marital_status` tinyint(1) DEFAULT NULL,
   `police_case` tinyint(1) DEFAULT NULL,
-  `profession` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profession_type` enum('none','student','employee','other') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profession_name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `gender` enum('male','female') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `age` int(2) DEFAULT NULL,
   `blood` char(2) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -109,10 +120,10 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `role`, `email`, `username`, `password`, `identity_type`, `identity_number`, `religion`, `full_name`, `birthday`, `birthplace`, `father_name`, `mother_name`, `ethnic_group`, `education`, `marital_status`, `police_case`, `profession`, `gender`, `age`, `blood`, `phone`, `address`, `sender`, `sender_name`, `photo`, `push_notif`, `status`, `registration_time`) VALUES
-(1, 'admin', 'admin@rekammedis.com', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', NULL, NULL, NULL, 'ADMIN', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'active', NULL),
-(2, 'dokter', 'dokter@rekammedis.com', 'dokter', '9d2878abdd504d16fe6262f17c80dae5cec34440', NULL, NULL, NULL, 'Dokter', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'active', NULL),
-(3, 'pasien', 'pasien@rekammedis.com', 'pasien', '2d64647e07ad6d7fdc36818a3f93a0c8a054bd18', NULL, NULL, NULL, 'Pasien', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'male', 40, 'AB', NULL, NULL, NULL, NULL, NULL, '{\"endpoint\":\"https://fcm.googleapis.com/fcm/send/c3McdAQivlQ:APA91bEf68H9PkvddQYW9utQBlBbNlk9vAIqIT1GOEz68IalTNPu5_VAxnBeHlH4gDoremFAoKPc10ZGMVRsPduaeQHlkAQtTUWPEwnrYPPeeNbWEY-pVXhqTYt4Zw8rPTd63jPW-2mK\",\"expirationTime\":null,\"keys\":{\"p256dh\":\"BMiY__PNbm0UZAvI3ezDEWLcl5ElERVPLnGSDBkuA9Fjug5YGCdgq67oJ6BeZLhmZO9EWSmI8iYYOlnoLbBo7yQ\",\"auth\":\"Xtompy90Oo6huh2jFtnkHw\"}}', 'active', '2022-02-20 21:56:13');
+INSERT INTO `user` (`id`, `role`, `email`, `username`, `password`, `identity_type`, `identity_number`, `religion`, `full_name`, `birthday`, `birthplace`, `father_name`, `mother_name`, `ethnic_group`, `education`, `marital_status`, `police_case`, `profession_type`, `profession_name`, `gender`, `age`, `blood`, `phone`, `address`, `sender`, `sender_name`, `photo`, `push_notif`, `status`, `registration_time`) VALUES
+(1, 'admin', 'admin@rekammedis.com', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', NULL, NULL, NULL, 'ADMIN', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'active', NULL),
+(2, 'dokter', 'dokter@rekammedis.com', 'dokter', '9d2878abdd504d16fe6262f17c80dae5cec34440', NULL, NULL, NULL, 'Dokter', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'active', NULL),
+(3, 'pasien', 'pasien@rekammedis.com', 'pasien', '2d64647e07ad6d7fdc36818a3f93a0c8a054bd18', NULL, NULL, NULL, 'Pasien', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'active', '2022-02-20 21:56:13');
 
 --
 -- Indexes for dumped tables
