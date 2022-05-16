d1987950a34808abf9912248cae553d158f08095
agoenks29D <agungmasda29@gmail.com>
2022-05-17 01:36:00 +0700

    latest update

--- application/views/admin/pasien/self_edit.php
+++ application/views/admin/pasien/self_edit.php
@@ -10,80 +10,223 @@
 		<form method="POST" action="<?= base_url($this->router->fetch_class().'/data_pasien/edit/'.$data->id) ?>">
 		<div class="box-body">
 			<div class="row">
-				<div class="col-lg-6">
+				<div class="col-lg-8">
 					<div class="form-group">
 						<label>Nama Lengkap</label>
 						<input class="form-control" type="text" name="full_name" placeholder="Nama Lengkap" value="<?= set_value('full_name', $data->full_name) ?>">
-						<?php echo form_error('full_name', '<span class="help-block error">', '</span>'); ?>
+						<?= form_error('full_name', '<span class="help-block error">', '</span>'); ?>
 					</div>
-					<div class="form-group">
-						<label>Email</label>
-						<input class="form-control" type="text" name="email" placeholder="Email" value="<?= set_value('email', $data->email) ?>">
-						<?php echo form_error('email', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="form-group">
+								<label>Kartu Identitas</label>
+								<select class="form-control" name="identity_type">
+									<option value="KTP" <?= set_value('identity_type', $data->identity_type) == 'KTP' ? 'selected' : '' ?>>KTP</option>
+									<option value="SIM" <?= set_value('identity_type', $data->identity_type) == 'SIM' ? 'selected' : '' ?>>SIM</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="form-group">
+								<label>Nomor Identitas</label>
+								<input class="form-control" type="text" name="identity_number" placeholder="Nomor Identitas" value="<?= set_value('identity_number', $data->identity_number) ?>">
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Nama Pengguna</label>
-						<input class="form-control" type="text" name="username" placeholder="Nama Pengguna" value="<?= set_value('username', $data->username) ?>">
-						<?php echo form_error('username', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="form-group">
+								<label>Nama Ibu</label>
+								<input class="form-control" type="text" name="mother_name" placeholder="Nama Ibu" value="<?= set_value('mother_name', $data->mother_name) ?>">
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="form-group">
+								<label>Nama Ayah</label>
+								<input class="form-control" type="text" name="father_name" placeholder="Nama Ayah" value="<?= set_value('father_name', $data->father_name) ?>">
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Jenis Kelamin</label>
-						<select class="form-control" name="gender">
-							<option value="">- Pilih Jenis Kelamin -</option>
-							<option value="male" <?= set_value('gender', $data->gender) == 'male'?'selected':'' ?>>Laki-laki</option>
-							<option value="female" <?= set_value('gender', $data->gender) == 'female'?'selected':'' ?>>Perempuan</option>
-						</select>
-						<?php echo form_error('gender', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Tempat Lahir</label>
+								<input class="form-control" type="text" name="birthplace" placeholder="Tempat Lahir" value="<?= set_value('birthplace', $data->birthplace) ?>">
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Tanggal Lahir</label>
+								<input class="form-control dateonly" type="text" name="birthday" placeholder="Tanggal Lahir" value="<?= set_value('birthday', nice_date($data->birthday, 'd/m/Y')) ?>">
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Jenis Kelamin</label>
+								<select class="form-control" name="gender">
+									<option value="">- Pilih Jenis Kelamin -</option>
+									<option value="male" <?= set_value('gender', $data->gender) == 'male'?'selected':'' ?>>Laki-laki</option>
+									<option value="female" <?= set_value('gender', $data->gender) == 'female'?'selected':'' ?>>Perempuan</option>
+								</select>
+								<?= form_error('gender', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Golongan Darah</label>
-						<select class="form-control" name="blood">
-							<option value="">- Pilih Golongan Darah -</option>
-							<option value="A" <?= set_value('blood', $data->blood) == 'A' ? 'selected':'' ?>>A</option>
-							<option value="B" <?= set_value('blood', $data->blood) == 'B' ? 'selected':'' ?>>B</option>
-							<option value="AB" <?= set_value('blood', $data->blood) == 'AB' ? 'selected':'' ?>>AB</option>
-							<option value="O" <?= set_value('blood', $data->blood) == 'O' ? 'selected':'' ?>>O</option>
-							<option value="A+" <?= set_value('blood', $data->blood) == 'A+' ? 'selected':'' ?>>A+</option>
-							<option value="B+" <?= set_value('blood', $data->blood) == 'B+' ? 'selected':'' ?>>B+</option>
-							<option value="AB+" <?= set_value('blood', $data->blood) == 'AB+' ? 'selected':'' ?>>AB+</option>
-							<option value="O+" <?= set_value('blood', $data->blood) == 'O+' ? 'selected':'' ?>>O+</option>
-						</select>
-						<?php echo form_error('blood', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Usia</label>
+								<input class="form-control" type="number" name="age" placeholder="Usia" onKeyPress="if(this.value.length == 2) return false;" value="<?= set_value('age', $data->age) ?>">
+								<?= form_error('age', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Golongan Darah</label>
+								<select class="form-control" name="blood">
+									<option value="">- Pilih Golongan Darah -</option>
+									<option value="A" <?= set_value('blood', $data->blood) == 'A' ? 'selected':'' ?>>A</option>
+									<option value="B" <?= set_value('blood', $data->blood) == 'B' ? 'selected':'' ?>>B</option>
+									<option value="AB" <?= set_value('blood', $data->blood) == 'AB' ? 'selected':'' ?>>AB</option>
+									<option value="O" <?= set_value('blood', $data->blood) == 'O' ? 'selected':'' ?>>O</option>
+									<option value="A+" <?= set_value('blood', $data->blood) == 'A+' ? 'selected':'' ?>>A+</option>
+									<option value="B+" <?= set_value('blood', $data->blood) == 'B+' ? 'selected':'' ?>>B+</option>
+									<option value="AB+" <?= set_value('blood', $data->blood) == 'AB+' ? 'selected':'' ?>>AB+</option>
+									<option value="O+" <?= set_value('blood', $data->blood) == 'O+' ? 'selected':'' ?>>O+</option>
+								</select>
+								<?= form_error('blood', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Agama</label>
+								<select class="form-control" name="religion">
+									<option value="islam" <?= set_value('religion', $data->religion) == 'islam' ? 'selected' : '' ?>>Islam</option>
+									<option value="kristen" <?= set_value('religion', $data->religion) == 'kristen' ? 'selected' : '' ?>>Kristen</option>
+									<option value="khatolik" <?= set_value('religion', $data->religion) == 'khatolik' ? 'selected' : '' ?>>Khatolik</option>
+									<option value="hindu" <?= set_value('religion', $data->religion) == 'hindu' ? 'selected' : '' ?>>Hindu</option>
+									<option value="budha" <?= set_value('religion', $data->religion) == 'budha' ? 'selected' : '' ?>>Budha</option>
+								</select>
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Kontak</label>
-						<input class="form-control" type="text" name="phone" placeholder="Kontak" value="<?= set_value('phone', $data->phone) ?>">
-						<?php echo form_error('phone', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Suku</label>
+								<input class="form-control" type="text" name="ethnic_group" placeholder="Suku" value="<?= set_value('ethnic_group', $data->ethnic_group) ?>">
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Pendidikan</label>
+								<select class="form-control" name="education">
+									<option value="NONE" <?= set_value('education', $data->education) == 'NONE' ? 'selected' : '' ?>>Tidak Sekolah</option>
+									<option value="SD" <?= set_value('education', $data->education) == 'SD' ? 'selected' : '' ?>>SD</option>
+									<option value="SLTP" <?= set_value('education', $data->education) == 'SLTP' ? 'selected' : '' ?>>SLTP</option>
+									<option value="SLTA" <?= set_value('education', $data->education) == 'SLTA' ? 'selected' : '' ?>>SLTA</option>
+									<option value="D3" <?= set_value('education', $data->education) == 'D3' ? 'selected' : '' ?>>D3</option>
+									<option value="S1" <?= set_value('education', $data->education) == 'S1' ? 'selected' : '' ?>>S1</option>
+									<option value="S2" <?= set_value('education', $data->education) == 'S2' ? 'selected' : '' ?>>S2</option>
+									<option value="S3" <?= set_value('education', $data->education) == 'S3' ? 'selected' : '' ?>>S3</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Status Menikah</label>
+								<select name="marital_status" class="form-control">
+									<option value="1" <?= set_value('marital_status', $data->marital_status) == '1' ? 'selected' : '' ?>>Menikah</option>
+									<option value="2" <?= set_value('marital_status', $data->marital_status) == '2' ? 'selected' : '' ?>>Belum Menikah</option>
+									<option value="3" <?= set_value('marital_status', $data->marital_status) == '3' ? 'selected' : '' ?>>Duda</option>
+									<option value="4" <?= set_value('marital_status', $data->marital_status) == '4' ? 'selected' : '' ?>>Janda</option>
+								</select>
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Alamat</label>
-						<input class="form-control" type="text" name="address" placeholder="Alamat" value="<?= set_value('address', $data->address) ?>">
-						<?php echo form_error('address', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Pekerjaan</label>
+								<select name="profession_type" class="form-control">
+									<option value="none" <?= set_value('profession_type', $data->profession_type) == 'none' ? 'selected' : '' ?>>Tidak Bekerja</option>
+									<option value="student" <?= set_value('profession_type', $data->profession_type) == 'student' ? 'selected' : '' ?>>Pelajar</option>
+									<option value="employee" <?= set_value('profession_type', $data->profession_type) == 'employee' ? 'selected' : '' ?>>Karyawan</option>
+									<option value="other" <?= set_value('profession_type', $data->profession_type) == 'other' ? 'selected' : '' ?>>Lainnya</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Deskrpsi Pekerjaan</label>
+								<input class="form-control" type="text" name="profession_name" placeholder="Deskrpsi Pekerjaan" value="<?= set_value('profession_name', $data->profession_name) ?>">
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Kasus Polisi</label>
+								<select name="police_case" class="form-control">
+									<option value="1" <?= set_value('police_case', $data->police_case) == '1' ? 'selected' : '' ?>>Ya</option>
+									<option value="2" <?= set_value('police_case', $data->police_case) == '2' ? 'selected' : '' ?>>Tidak</option>
+								</select>
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Usia</label>
-						<input class="form-control" type="number" name="age" placeholder="Usia" onKeyPress="if(this.value.length == 2) return false;" value="<?= set_value('age', $data->age) ?>">
-						<?php echo form_error('age', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Email</label>
+								<input class="form-control" type="text" name="email" placeholder="Email" value="<?= set_value('email', $data->email) ?>">
+								<?= form_error('email', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Username</label>
+								<input class="form-control" type="text" name="username" placeholder="Username" value="<?= set_value('username', $data->username) ?>">
+								<?= form_error('username', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Kata Sandi</label>
+								<input class="form-control" type="text" name="password" placeholder="Kata Sandi" value="">
+								<?= form_error('password', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
 					</div>
-					<div class="form-group">
-						<label>Kata Sandi</label>
-						<input class="form-control" type="text" name="password" placeholder="Kata Sandi" value="">
-						<?php echo form_error('password', '<span class="help-block error">', '</span>'); ?>
-					</div>
-					<div class="form-group">
-						<label>Status</label>
-						<select class="form-control" name="status">
-							<option value="">- Pilih Status Akun -</option>
-							<option value="active" <?= set_value('status', $data->status) == 'active'?'selected':'' ?>>Aktif</option>
-							<option value="non-active" <?= set_value('status', $data->status) == 'non-active'?'selected':'' ?>>Non-Aktif</option>
-						</select>
-						<?php echo form_error('status', '<span class="help-block error">', '</span>'); ?>
+					<div class="row">
+						<div class="col-lg-4">
+							<label>Dikirim Oleh</label>
+							<select class="form-control" name="sender">
+								<option value="voluntary" <?= set_value('sender', $data->sender) == 'voluntary' ? 'selected' : '' ?>>Sukarela</option>
+								<option value="sender_1" <?= set_value('sender', $data->sender) == 'sender_1' ? 'selected' : '' ?>>BNN/BNNP</option>
+								<option value="sender_2" <?= set_value('sender', $data->sender) == 'sender_2' ? 'selected' : '' ?>>Balai Rehabilitasi</option>
+								<option value="sender_3" <?= set_value('sender', $data->sender) == 'sender_3' ? 'selected' : '' ?>>RS/Panti/Puskesmas/Lembaga</option>
+								<option value="other" <?= set_value('sender', $data->sender) == 'other' ? 'selected' : '' ?>>Lainnya</option>
+							</select>
+						</div>
+						<div class="col-lg-4 sender_name <?= (!empty($data->sender_name) OR $data->sender == 'voluntary') ? 'hidden' : '' ?>">
+							<label>Pengirim</label>
+							<input type="text" name="sender_name" class="form-control" placeholder="Pengirim" value="<?= set_value('sender_name', $data->sender_name) ?>">
+						</div>
+						<div class="col-lg-4">
+							<div class="form-group">
+								<label>Status</label>
+								<select class="form-control" name="status">
+									<option value="">- Pilih Status Akun -</option>
+									<option value="active" <?= set_value('status', $data->status) == 'active'?'selected':'' ?>>Aktif</option>
+									<option value="non-active" <?= set_value('status', $data->status) == 'non-active'?'selected':'' ?>>Non-Aktif</option>
+								</select>
+								<?= form_error('status', '<span class="help-block error">', '</span>'); ?>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="box-footer">
-			<a href="<?= base_url($this->router->fetch_class().'/dokter') ?>" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
+			<a href="<?= base_url($this->router->fetch_class().'/pasien') ?>" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
 			<button class="btn btn-success" type="submit" class="form-control">Simpan <i class="fa fa-save"></i></button>
 		</div>
 		</form>
--- application/views/admin/pasien/self_view.php
+++ application/views/admin/pasien/self_view.php
@@ -15,39 +15,128 @@
 		</div>
 		<div class="box-body">
 			<div class="row">
-				<div class="col-lg-6">
+				<div class="col-lg-4">
 					<table class="table table-hover table-striped table-bordered">
-						<tr>
-							<td>Nama Pasien</td><td><?= $data->full_name ?></td>
-						</tr>
-						<tr>
-							<td>Email</td><td><?= $data->email ?></td>
-						</tr>
-						<tr>
-							<td>Nama Pengguna</td><td><?= $data->username ?></td>
-						</tr>
-						<tr>
-							<td>Jenis Kelamin</td><td><?= ($data->gender == 'male')?'Laki-laki':'Perempuan' ?></td>
-						</tr>
-						<tr>
-							<td>Waktu Registrasi</td><td><?= (!empty($data->registration_time))?nice_date($data->registration_time, 'd-m-Y'):'-' ?></td>
-						</tr>
-						<tr>
-							<td>Kontak</td><td><?= $data->phone ?></td>
-						</tr>
-						<tr>
-							<td>Alamat</td><td><?= $data->address ?></td>
-						</tr>
-						<tr>
-							<td>Status</td>
-							<td>
-							<?php if ($data->status == 'active'): ?>
-								<button class=" btn btn-xs btn-success">Aktif</button>
-							<?php else: ?>
-								<button class=" btn btn-xs btn-danger">Non-Aktif</button>
+						<thead>
+							<th colspan="2" class="text-center">Informasi Akun</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Email</td><td><?= $data->email ?></td>
+							</tr>
+							<tr>
+								<td>Username</td><td><?= $data->username ?></td>
+							</tr>
+							<tr>
+								<td>Status</td>
+								<td>
+								<?php if ($data->status == 'active'): ?>
+									<button class=" btn btn-xs btn-success">Aktif</button>
+								<?php else: ?>
+									<button class=" btn btn-xs btn-danger">Non-Aktif</button>
+								<?php endif; ?>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-lg-4">
+					<table class="table table-hover table-striped table-bordered">
+						<thead>
+							<th colspan="2" class="text-center">Informasi Pasien</th>
+						</thead>
+						<tbody>
+							<?php if (!empty($data->identity_type)) : ?>
+							<tr>
+								<td><?= $data->identity_type ?></td><td><?= exists_value($data->identity_number) ?></td>
+							</tr>
 							<?php endif; ?>
-							</td>
-						</tr>
+							<tr>
+								<td>Nama Pasien</td><td><?= $data->full_name ?></td>
+							</tr>
+							<tr>
+								<td>Email</td><td><?= $data->email ?></td>
+							</tr>
+							<tr>
+								<td>Username</td><td><?= $data->username ?></td>
+							</tr>
+							<tr>
+								<td>Jenis Kelamin</td><td><?= ($data->gender == 'male')?'Laki-laki':'Perempuan' ?></td>
+							</tr>
+							<?php if (!empty($data->birthplace)) : ?>
+							<tr>
+								<td>Tempat/Tanggal Lahir</td><td><?= $data->birthplace.' '.(!empty($data->birthdate) ? nice_date($data->birthdate, 'd-m-Y') : '') ?></td>
+							</tr>
+							<?php endif; ?>
+							<tr>
+								<td>Usia</td><td><?= exists_value($data->age) ?></td>
+							</tr>
+							<tr>
+								<td>Golongan Darah</td><td><?= exists_value($data->blood) ?></td>
+							</tr>
+							<tr>
+								<td>Profesi</td><td><?= exists_value($data->profession_type, '').' '.exists_value($data->profession_name, '') ?></td>
+							</tr>
+							<tr>
+								<td>Status Pernikahan</td>
+								<td>
+								<?php
+								switch ($data->marital_status)
+								{
+									case '1':
+										echo 'Menikah';
+									break;
+
+									case '2':
+										echo 'Belum Menikah';
+									break;
+
+									case '3':
+										echo 'Duda';
+									break;
+
+									case '4':
+										echo 'Janda';
+									break;
+
+									default:
+										echo '-';
+									break;
+								}
+								?>
+								</td>
+							</tr>
+							<tr>
+								<td>Alamat</td><td><?= exists_value($data->address) ?></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-lg-4">
+					<table class="table table-hover table-striped table-bordered">
+						<thead>
+							<th colspan="2" class="text-center">Informasi Tambahan</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Orangtua Laki-laki</td><td><?= exists_value($data->father_name) ?></td>
+							</tr>
+							<tr>
+								<td>Orangtua Perempuan</td><td><?= exists_value($data->mother_name) ?></td>
+							</tr>
+							<tr>
+								<td>Suku</td><td><?= exists_value($data->ethnic_group) ?></td>
+							</tr>
+							<tr>
+								<td>Agama</td><td><?= !empty($data->religion) ? ucfirst($data->religion) : '-' ?></td>
+							</tr>
+							<tr>
+								<td>Pendidikan</td><td><?= exists_value($data->education) ?></td>
+							</tr>
+							<tr>
+								<td>Kasus Polisi</td><td><?= exists_value($data->police_case) == 1 ? 'Ya' : 'Tidak' ?></td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 			</div>
@@ -55,6 +144,7 @@
 		<div class="box-footer">
 			<a href="<?= base_url($this->router->fetch_class()) ?>" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
 			<a href="<?= base_url($this->router->fetch_class().'/print_report/'.$data->id) ?>" class="btn btn-info"><i class="fa fa-print"></i> Cetak Laporan</a>
+			<a href="<?= base_url($this->router->fetch_class().'/data_pasien/edit/'.$data->id) ?>" class="btn btn-primary pull-right"><i class="fa fa-edit"></i> Sunting Informasi</a>
 		</div>
 	</div>
 </section>
